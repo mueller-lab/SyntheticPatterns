57eb15eb0db1292d36f2d969492f2731c0919937
mueller-lab <32100660+mueller-lab@users.noreply.github.com>
2024-12-13 19:24:15 +0100

    Add files via upload

--- DataAnalysis/PythonScripts/travelingWavesAnalysis/meanDynamicsPlot.py
+++ DataAnalysis/PythonScripts/travelingWavesAnalysis/meanDynamicsPlot.py
@@ -20,55 +20,27 @@ def smoothConvolve(data, sz = 5, mode = "same"):
     smthData = np.convolve(data, kernel, mode= mode)
     return smthData
 
-def plot_dynamics(posL, chnls, exDates, timeNum):
-    # # make colormap
-    viridis = cm.get_cmap('viridis', 256)
-    cmapV = viridis(np.linspace(0, 1, 5))
-
-    magma = cm.get_cmap('magma', 256)
-    cmapM = magma(np.linspace(0, 1, 5))
-
+def plot_dynamics(chnls, timeNum):
     # make a dict to assign cmap to ch
-    clrDict = {1:cmapV[3], 2:cmapM[3]}
+    clrDict = {1:'g', 2:'r'}
     lblDict = {1:'sfGFP', 2:'mCherry'}
 
     # make timeAx
     timeAx = [i*tPerd for i in range(timeNum)] # time in hours
-    timeAx_min = 60*np.array(timeAx)
 
     # make figure
-    fig, ax  = plt.subplots(nrows = 1, ncols = 1, figsize= (2.4,1.6))
-
-    chDict = {}
-    for ch in chnls:
-        yPosAll = []
-        for pos in posL:
-            for exptDate in exDates:
-            # read data and make pandas df
-                fName = f"XY{pos}_CH{ch}_measure.csv"
-                fPath = inPath/exptDate/'entLawn'/fName
-                if fPath.is_file():
-                    data_df = pd.read_csv(str(fPath),header=0, index_col = 0)
-
-                    y =data_df['Mean']
-                    y = y[:timeNum] # set the last timeNum
-                    y = (y-np.min(y))/(np.max(y)-np.min(y))
-                    yPosAll.append(y)
-        chDict[ch] = np.array(yPosAll)
+    fig, ax  = plt.subplots(nrows = 1, ncols = 1, figsize= (2.5,1.5))
 
     for ch in chnls:
-        y= chDict[ch]
-        yAvg = np.mean(y, axis = 0)
-        err = np.std(y, axis=0)
-        ax.plot(timeAx, yAvg, marker= ".", markersize=1, color =clrDict[ch], label = lblDict[ch], linewidth = 1)
+        # read data and make pandas df
+        fName = f"CH{ch}_measure.csv"
+        fPath = inPath/fName
+        data_df = pd.read_csv(str(fPath),header=0, index_col = 0)
 
-        # markers, caps, bars = ax.errorbar(timeAx,yAvg, yerr= err, fmt="none", capsize = 0.5, \
-        # capthick= 1.0, c = clrDict[ch], elinewidth = 1.)
-        #
-        # [bar.set_alpha(0.2) for bar in bars] # to make the errorbars transparent
-        # [cap.set_alpha(0.2) for cap in caps]
+        y =data_df['Mean']
+        y = (y-10.0)/(np.max(y)-10)
 
-        plt.fill_between(timeAx, yAvg-err, yAvg+err, alpha = 0.2, color = clrDict[ch], edgecolor= 'face')
+        ax.plot(timeAx, y, marker= ".", markersize=1, color =clrDict[ch], label = lblDict[ch], linewidth = 1)
 
     ax.grid(False)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
@@ -77,11 +49,10 @@ def plot_dynamics(posL, chnls, exDates, timeNum):
     ax.set_ylabel("Mean intensity (A.U.)")
     # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-    ax.set_ylim([0, 1.06])
+    ax.set_ylim([0, 1.02])
     ax.set_xlim(timeAx[0], timeAx[-1])
 
     ax.tick_params(axis='both', which='major', labelsize=6)
-    # ax.set_aspect('equal')
 
     plt.tight_layout()
 
@@ -114,12 +85,9 @@ if __name__=="__main__":
     outPath.mkdir(parents=True, exist_ok=True)
 
     tPerd = 0.25
-    timeNum = 97
+    timeNum = 105
 
     chnls = [1,2]
-    posL=['12', '16']
-    exDates = ['20230719', '20230720', '20230721']
-
-    plot_dynamics(posL, chnls, exDates, timeNum)
+    plot_dynamics(chnls, timeNum)
 
     print("analysis is complete.")
