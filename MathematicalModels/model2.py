b3ff292ccb887d117c449dfdb9a03e0b5145c3ec
mueller-lab <32100660+mueller-lab@users.noreply.github.com>
2024-12-14 16:19:34 +0100

    Update model2.py

--- MathematicalModels/model2.py
+++ MathematicalModels/model2.py
@@ -357,7 +357,7 @@ if __name__=="__main__":
     datetime_str=(now.strftime("%Y%m%d_")) # %H%M%S_
 
     #make output directory
-    dirName=str(datetime_str+ "mod2_AHL12sensor_doseRe_Fig3") # the positive loop model
+    dirName=str(datetime_str+ "mod2_AHL12sensor_doseRe") # the positive loop model
 
     resultPath=cwdPath/'data_1D'/dirName
     resultPath.mkdir(mode=0o777, parents=True, exist_ok=True)
