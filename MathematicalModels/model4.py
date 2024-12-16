de9641d65b795fc735a510ae8359cf26426a14ea
mueller-lab <32100660+mueller-lab@users.noreply.github.com>
2024-12-16 16:39:15 +0100

    Update model4.py

--- MathematicalModels/model4.py
+++ MathematicalModels/model4.py
@@ -177,7 +177,7 @@ class RD_simulate(object):
         to make time-series plots of the variable output
         """
         # make a color dict to assign colors to typeId
-        viridis = cm.get_cmap('magma', 256)
+        viridis = matplotlib.colormaps.get_cmap('magma', 256)
         cmap0 = viridis(np.linspace(0, 1, 1+len(self.ahl12List)))
 
         for vName in self.vNames:
